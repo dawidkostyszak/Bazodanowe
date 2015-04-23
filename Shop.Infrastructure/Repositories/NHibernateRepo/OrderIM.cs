e0ea2a4259967cabb67a139c48c79040dba396b5
Dawid Kostyszak <d.kostyszak@gmail.com>
2015-04-23 13:44:17 +0200

    add order IM

---
+++ Shop.Infrastructure/Repositories/NHibernateRepo/OrderIM.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using NHibernate.Linq;
+using Shop.Domain.Model.Order;
+using Shop.Domain.Model.Order.Repositories;
+
+namespace Shop.Infrastructure.Repositories.NHibernateRepo
+{
+    public class OrderIM : IOrderRepository
+    {
+        public void Insert(Order order)
+        {
+            using (var session = NHibernateHelper.OpenSession())
+            {
+                using (var transaction = session.BeginTransaction())
+                {
+                    session.Insert(order);
+                    transaction.Commit();
+                }
+            }
+        }
+
+        public void Delete(int id)
+        {
+            using (var session = NHibernateHelper.OpenSession())
+            {
+                using (var transaction = session.BeginTransaction())
+                {
+                    var orderQuery = (from o in session.Query<Order>() where o.Id == id select o).Single();
+                    session.Delete(orderQuery);
+                    transaction.Commit();
+                }
+            }
+        }
+
+        public Order Find(int id)
+        {
+            using (var session = NHibernateHelper.OpenSession())
+            {
+                return (from o in session.Query<Order>() where o.Id == id select o).Single();
+            }
+        }
+
+        public List<Order> FindAll()
+        {
+            using (var session = NHibernateHelper.OpenSession())
+            {
+                return session.Query<Order>().ToList();
+            }
+        }
+
+        public List<Order> FindByCity(string city)
+        {
+            using (var session = NHibernateHelper.OpenSession())
+            {
+                return (from o in session.Query<Order>() where o.Customer.Address.City == city select o).ToList();
+            }
+        }
+
+        public List<Order> FindByUser(int customerId)
+        {
+            using (var session = NHibernateHelper.OpenSession())
+            {
+                return (from o in session.Query<Order>() where o.Customer.Id == customerId select o).ToList();
+            }
+        }
+    }
+}
